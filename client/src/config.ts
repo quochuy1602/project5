3ee3935014faa09883583365cd4f4d63c2f7ff35
Quoc Huy <quochuy1602@gmail.com>
2022-09-21 00:19:01 +0700

    Add code

---
+++ client/src/config.ts
@@ -0,0 +1,7 @@
+export const apiEndpoint = `https://yewoqjmn5a.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-48rwbbfq.us.auth0.com',
+  clientId: 'CMfgYVZdalIT2h9j9St1i6u1mPH1SMT7',
+  callbackUrl: 'http://localhost:3000/callback'
+}
